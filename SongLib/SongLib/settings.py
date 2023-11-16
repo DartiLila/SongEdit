9653dabcac71fa88f7cd871b33b7568f422eb189
DartiLila <95767904+DartiLila@users.noreply.github.com>
2023-11-16 14:14:43 +0100

    Database

--- SongLib/SongLib/settings.py
+++ SongLib/SongLib/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'SongLib.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SONGLIB',
+        'USER': 'admin',
+        'PASSWORD': 'admin1234',
+        'HOST': 'host-riki',
+        'PORT': '3306',
     }
 }
 
